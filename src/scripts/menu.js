60fd2cbe441640c0cd4609b44c2adf08ffae832b
Brayden Grotegut <bgrotegut21@gmail.com>
2021-10-10 18:33:41 -0600

    refactoring ugly code

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,12 @@
-import arrowDownImage from "../arrowDrop.svg";
-import arrowUpImage from "../arrowUp.svg";
-import {createElementsChildren, setToElement} from "elementEvents.js";
+import arrowDownImage from "../images/arrowDrop.svg";
+import arrowUpImage from "../images/arrowUp.svg";
+import hamburgerImage from "../images/hamburger.svg";
+import cheeseBurgerImage from "../images/cheeseburger.svg";
+import chickenSandwichImage from "../image/chickenSandwhich.svg";
+import doubleCheeseBurgerImage from "../images/"
+
+import {createElementsChildren, setToElement} from "./elementEvents.js";
+
 
 
 let menuInterface = (
@@ -11,10 +17,13 @@ let menuInterface = (
         {class: "dessertCategory", text: "Sandwiches"}, ]
         
 
-        function setCategorySelction(categorySelction, currentCategory, arrowDown, arrowUp,currentCategoryText, categoryButtons, categories){
+        function setCategorySelction(categorySelection, currentCategory, arrowDown, arrowUp,currentCategoryText, categoryButtons, categories){
+            let currentCategoryItems = [arrowDown, arrowUp, currentCategoryText];
+            setToElement(currentCategory,currentCategoryItems);
+            setToElement(categoryButtons,categories); 
 
-
-            
+            let categoryItems = [currentCategory, categoryButtons];
+            setToElement(categorySelection,categoryItems);
         }
 
 
@@ -41,7 +50,30 @@ let menuInterface = (
             categoryButtons.setAttribute("class", "categoryButtons");
         
             let categories = categoryArray.map(object => createElementsChildren("button",object.class,object.text));
-            setCategorySelction(categorySelction,currentCategory,arrowDown,arrowUp,currentCategoryText,categoryButtons,categories);
+            setCategorySelction(categorySelection,currentCategory,arrowDown,arrowUp,currentCategoryText,categoryButtons,categories);
+        }
+
+        function createSections(className){
+            let sections = document.createElement("div");
+            sections.setAttribute("class",className);
+            return sections;
+
+        }
+
+        function createSections(imageClass,image, divClass,h2Text, pText,){
+            let section = document.createElement("div");
+            section.setAttribute("class","section");
+
+            let image = do
+
+        }
+
+        function createAllSections(){
+            let sections = ["sections sandwichSection", "sections sidesSection","sections desserSection" ];
+            let sectionsElement = sections.map(section => createSections(section));
+
+
+
         }
 
     }
